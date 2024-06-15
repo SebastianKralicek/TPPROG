89176956fe5e5d2177eeb5c67d3934ac8edb2648
SebastianKralicek <skralicek@udesa.edu.ar>
2024-06-15 16:40:25 -0300

    corregi ciertos problemas

--- TRABAJO/js/index.js
+++ TRABAJO/js/index.js
@@ -1,3 +1,5 @@
+/* SECCION DE HOMBRES*/
+
 let hombre = document.querySelector(".hombresCat")
 let product = "";
 
@@ -18,7 +20,6 @@ product = `
             <h3 class="textotitulo">${ropa.title}</h3>   
             <img src="${ropa.image}" class="remeranegra">
     <section class="precioydesc">
-        <p class="desc">${ropa.description}</p>
         <p class="precio1"><b>$${ropa.price}</b></p>
         <article class="precio">
             <a href="producto.html"><button class="botoncomprar">Saber Mas</button></a>
@@ -30,30 +31,5 @@ hombre.innerHTML += product;
     }
 }) 
 
-    
 
-
-/* let productos = document.querySelector("#productos")
-
-function ProductosIndex(){
-   
-    productos.forEach(producto =>{
-
-        let section = document.createElement("section")
-        section.classList.add("producto") 
-        section.innerHTML = `
-        <article class="descripcionprod">
-            <h3 class="textotitulo">REMERAS</h3>   
-            <img src="${producto.image}" class="remeranegra">
-    <section class="precioydesc">
-        <p class="desc">${producto.description}</p>
-        <p class="precio1"><b>${producto.price}</b></p>
-        <article class="precio">
-            <a href="producto.html"><button class="botoncomprar" id="${producto.id}>Saber Mas</button></a>
-            <a href="producto.html"><button class="botoncomprar">Agregar al Carrito</button></a>
-        </article> `
-    
-        productos.append(section)
-})
-}
-*/
+            
