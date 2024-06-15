bc44e0cab534d82027273f0f456925664f944ce8
biehay <bhaymesbiedma@udesa.edu.ar>
2024-06-15 16:51:08 -0300

    js mujeres del home

--- TRABAJO/js/index.js
+++ TRABAJO/js/index.js
@@ -31,5 +31,37 @@ hombre.innerHTML += product;
     }
 }) 
 
+/*SECCION DE MUJERES*/
+
+let mujer = document.querySelector(".MujeresCat")
+let productMujer = ""
+
+fetch('https://fakestoreapi.com/products/category/women\'s clothing')
+            .then(function(response){
+             return response.json()
+            })
+
+.then(function(data){
+    console.log(data);
+for (i = 0; i < data.length; i++){
+    let ropamujer = data[i];
+productMujer = `
+<article class = "MujeresCat">
+        <section class="productos">
+            <article class="descripcionprod">
+                <h3 class="textotitulo">${ropamujer.title}</h3>   
+                <img src="${ropamujer.image}" class="remeranegra">
+        <section class="precioydesc">
+            <p class="precio1"><b>$${ropamujer.price}</b></p>
+            <article class="precio">
+                <a href="producto.html"><button class="botoncomprar" id="${ropamujer.id}">Saber Mas</button></a>
+                <a href="producto.html"><button class="botoncomprar">Agregar al Carrito</button></a>
+</article>
+`
+mujer.innerHTML += productMujer;
+
+}
+})
+
 
             
