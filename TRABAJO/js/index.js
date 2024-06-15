db5c96273fbef50bf576f67ae190c9d353019809
SebastianKralicek <skralicek@udesa.edu.ar>
2024-06-15 13:56:07 -0300

    Js de hombre hecho (problemitas por resolver)

--- TRABAJO/js/index.js
+++ TRABAJO/js/index.js
@@ -1,22 +1,32 @@
-let results = fetch('https://fakestoreapi.com/products')
-.then(function(res){
-    return res.json()
-})
+let hombre = document.querySelector(".hombresCat")
+let product = "";
+
+fetch('https://fakestoreapi.com/products/category/men\'s clothing')
+            .then(function(response){
+             return response.json()
+            })
+
+
 .then(function(data){
     console.log(data);
-    let results = document.querySelector("#productos")
     for (i = 0; i < data.length; i++){
-results.innerHTML = `
+        let ropa = data[i];
+product = `
+    <article class="hombresCat">
+    <section class="productos">
         <article class="descripcionprod">
-            <h3 class="textotitulo">${data[i].title}</h3>   
-            <img src="${data[i].image}" alt="${data[i].title} class="remeranegra">
+            <h3 class="textotitulo">${ropa.title}</h3>   
+            <img src="${ropa.image}" class="remeranegra">
     <section class="precioydesc">
-        <p class="desc">${data[i].description}</p>
-        <p class="precio1"><b>${data[i].price}</b></p>
+        <p class="desc">${ropa.description}</p>
+        <p class="precio1"><b>$${ropa.price}</b></p>
         <article class="precio">
             <a href="producto.html"><button class="botoncomprar">Saber Mas</button></a>
-            <a href="producto.html"><button class="botoncomprar">Agregar al Carrito</button></a>
+            <a href="producto.html"><button class="botoncomprar" id="${ropa.id}">Agregar al Carrito</button></a>
+        </article>
+    </section>
         </article> `
+hombre.innerHTML += product;
     }
 }) 
 
