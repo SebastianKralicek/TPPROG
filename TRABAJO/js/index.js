4f0ef9eca018e5aa1c936c44e385bf00e3ec6e0e
TomiCataldi <tcataldimayer@udesa.edu.ar>
2024-06-19 21:14:32 -0300

    seccion de joyeria hecha

--- TRABAJO/js/index.js
+++ TRABAJO/js/index.js
@@ -59,3 +59,34 @@ mujer.innerHTML += productMujer;
 }
 })
 
+// SECCION JOYERIA //
+            
+let joyas = document.querySelector(".JoyeriaCat")
+let productJoyas = ""
+fetch('https://fakestoreapi.com/products/category/jewelery')
+            .then(function(response){
+             return response.json()
+            })
+
+.then(function(data){
+console.log(data);
+for (i = 0; i < data.length; i++){
+productJoyas = 
+`
+<article class="JoyasCat">
+<section class="productos">
+ <article class="descripcionprod">
+            <h3 class="textotitulo">${data[i].title}</h3>   
+            <img src="${data[i].image}" class="remeranegra">
+    <section class="precioydesc">
+        <p class="precio1"><b>$${data[i].price}</b></p>
+        <article class="precio">
+            <a href="producto.html"><button class="botoncomprar" id="${data[i].id}">Saber Mas</button></a>
+        </article>
+</section>
+</article>
+`
+joyas.innerHTML += productJoyas;
+
+}
+})
