7659018350ce1270d4565b489dced96e44a621b3
SebastianKralicek <skralicek@udesa.edu.ar>
2024-06-21 20:09:34 -0300

    Carrito y finalizar compra HECHO

--- TRABAJO/js/carrito.js
+++ TRABAJO/js/carrito.js
@@ -1,6 +1,6 @@
-let CarritoEntero = document.querySelector(".seccion1-items")
-let FotoProducto = document.querySelector(".carrito-item")
-let DetallesDelItem = document.querySelector('.carrito-item-detalles')
+let CarritoEntero = document.querySelector(".carrito-item")
+let FotoProducto = ""
+let FinalizarCompra = document.querySelector("#Pagar")
 
 let DataLocal = localStorage.getItem("DatosDelLocal")
 
@@ -9,13 +9,14 @@ console.log(DataLocal);
 console.log(DataParse);
 
 
+
 if (DataLocal == null ){
     CarritoEntero.innerHTML = `
     <h3 class="NoProducts"> No tienes productos aqui </h3>
     `
 }
 else{
-    for (i = 0; i < DataParse.length; i++)
+    for (i = 0; i < DataParse.length; i++){
     fetch(`https://fakestoreapi.com/products/${DataParse[i].id}`) //Parecido a Producto
     .then(function(res){
         return res.json();
@@ -23,19 +24,24 @@ else{
 .then(function(data){
     console.log(data);
 let FotoProducto = `
-    <article class="carrito-item"> 
-        <img class="imagenes-carrito" src="${data[i].image}" alt="" width="200px">
+        <img class="imagenes-carrito" src="${data.image}" alt="" width="200px">
         <article class="carrito-item-detalles"> 
-            <p class="carrito-item-titulo-1">${data[i].title}</p>
-            <p class="carrito-item-precio-1">$${data[i].price}</p>
+            <p class="carrito-item-titulo-1">${data.title}</p>
+            <p class="carrito-item-precio-1">$${data.price}</p>
             <section class="filita-1"> 
         </article>
-    </article>
 `
-DetallesDelItem.innerHTML += FotoProducto;
+CarritoEntero.innerHTML += FotoProducto;
 })
 .catch(function(err){
     console.log(err);
 })
-
 }
+FinalizarCompra.addEventListener("click", function(){
+    localStorage.removeItem("DatosDelLocal");
+    alert("Gracias por su compra")
+})
+}
+
+
+
