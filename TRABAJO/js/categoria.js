11b9a333752af52630a78cc826a793014c9f96d2
biehay <bhaymesbiedma@udesa.edu.ar>
2024-06-23 20:45:40 -0300

    js categoria

--- TRABAJO/js/categoria.js
+++ TRABAJO/js/categoria.js
@@ -1,44 +1,53 @@
-
-let hombre = document.querySelector(".hombresCat")
-let product = "";
-
-fetch('https://fakestoreapi.com/products/category/men\'s clothing')
-            .then(function(response){
-             return response.json()
-            })
-
-
-.then(function(data){
-    console.log(data);
-    for (i = 0; i < data.length; i++){
-product = `
-    <article class="hombresCat">
-    <section class="productos">
-        <article class="descripcionprod">
-            <h3 class="textotitulo">${data[i].title}</h3>   
-            <a href="./producto.html?id=${data[i].id}"><img src="${data[i].image}" class="remeranegra"></a>
-    <section class="precioydesc">
-        <p class="precio1"><b>$${data[i].price}</b></p>
-        <article class="precio">
-            <a href="./producto.html?id=${data[i].id}"><button class="botoncomprar" id="${data[i].id}">Saber Mas</button></a>
-        </article>
-    </section>
-        </article> `
-hombre.innerHTML += product;
-    }
-})
-.catch(function(err){
-    console.log(err);
-})
-
 console.log(location.search);
 
 let queryString = location.search
 
-let queryStringObj = new URLSearchParams (queryString)
+let queryStringObj = new URLSearchParams(queryString)
 
-let id = queryStringObj.get ("id")
+let category = queryStringObj.get("category")
 
-console.log(id);
+console.log(category);
+            
+    let producctt = document.querySelector(".contenedoresIndexuno")
+    let especifico="";
+         
+    fetch ('https://fakestoreapi.com/products/category/men\'s clothing')
 
-fetch('')
+    .then (function(response){
+        return response.json();
+    })
+
+    .then (function(data){
+        console.log(data);
+especifico = <section class= "contenedoresIndexuno">
+        
+         for (let i=0; i<data.length; i++){
+            let stock = data [i];
+            console.log(stock);
+            console.log(stock.title);
+            console.log(stock.image);
+            console.log(stock.description);
+           
+
+            especifico=especifico+  `
+            <article class="hombresCat">
+            <section class="productos">
+                <article class="descripcionprod">
+                    <h3 class="textotitulo">${data[i].title}</h3>   
+                    <a href="./producto.html?id=${data[i].id}"><img src="${data[i].image}" class="remeranegra"></a>
+            <section class="precioydesc">
+                <p class="precio1"><b>$${data[i].price}</b></p>
+                <article class="precio">
+                    <a href="./producto.html?id=${data[i].id}"><button class="botoncomprar" id="${data[i].id}">Saber Mas</button></a>
+                </article>
+            </section>
+                </article> `;
+}
+        
+            especifico=especifico+ </section>
+            producctt.innerHTML = especifico;
+        }
+    )
+    .catch (function(err){
+        console.log(err);
+    })
