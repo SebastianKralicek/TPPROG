c4f90010b858dd5b5f4c225f46affeda825c30b2
TomiCataldi <tcataldimayer@udesa.edu.ar>
2024-06-23 20:29:48 -0300

    acomode el css de registro y corregi unas cosas en registro.js

--- TRABAJO/js/registro.js
+++ TRABAJO/js/registro.js
@@ -1,43 +1,43 @@
+
 document.querySelector('.formregistro').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    let email = document.querySelector('#emailregistro').value.trim();
-    let password = document.querySelector('#contraseñaregistro').value.trim();
-    let repeatPassword = document.querySelector('#repetircontraseñaregistro').value.trim();
-    let terms = document.querySelector('#terminosycondiciones').checked;
+    
+    let email = document.querySelector('#emailregistro')
+    let password = document.querySelector('#contraseñaregistro')
+    let repeatPassword = document.querySelector('#repetircontraseñaregistro')
+    let terms = document.querySelector('#terminosycondiciones')
+    let errorMessage = document.querySelector('.errormensaje')
 
-    let errorMessage = document.querySelector('.error-message2');
-    if (!errorMessage) {
-        errorMessage = document.createElement('p');
-        errorMessage.classList.add('error-message2');
-        document.querySelector('.formregistro').appendChild(errorMessage);
-    }
-    errorMessage.textContent = '';
 
     if (!email) {
-        errorMessage.textContent = 'Por favor complete el campo email';
+        errorMessage = document.querySelector("p").innerHTML = "Por favor complete el campo email";
         return;
     }
     
     if (!password) {
-        errorMessage.textContent = 'Por favor complete el campo contraseña';
+        errorMessage = document.querySelector("p").innerHTML = "Por favor complete el campo contraseña";
         return;
+    
     }
-
     if (password.length < 6) {
-        errorMessage.textContent = 'Debe ingresar al menos 6 caracteres';
+        errorMessage = 'Debe ingresar al menos 6 caracteres';
         return;
     }
 
     if (password !== repeatPassword) {
-        errorMessage.textContent = 'Las contraseñas no coinciden';
+        errorMessage = 'Las contraseñas no coinciden';
         return;
     }
 
     if (!terms) {
-        errorMessage.textContent = 'Por favor acepte los términos y condiciones';
+        errorMessage = 'Por favor acepte los términos y condiciones';
         return;
     }
 
     window.location.href = './login.html';
 });
+
+
+
+
