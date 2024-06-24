de95d1e623d18f0f50936553f7ab57ceb9d4b0d7
TomiCataldi <tcataldimayer@udesa.edu.ar>
2024-06-24 14:53:51 -0300

    acomode el css y avanze con el recover

---
+++ TRABAJO/js/recover.js
@@ -0,0 +1,7 @@
+let form = document.querySelector('.formrecover');
+
+let email = document.querySelector('#emailrecover');
+let RecoverPassword = document.querySelector('#PasswordRecover');
+
+let EmailError = document.querySelector('#EmailErrorRecover');
+let RecoverPasswordError = document.querySelector('#PasswordErrorRecover');
