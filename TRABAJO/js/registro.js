504c91431df4fcc0c49ccb8187c22169940eaa8c
TomiCataldi <tcataldimayer@udesa.edu.ar>
2024-06-23 23:38:49 -0300

    hice el registro.j (me falta chequear un error)

--- TRABAJO/js/registro.js
+++ TRABAJO/js/registro.js
@@ -1,43 +1,83 @@
+let form = document.querySelector('.formregistro');
 
-document.querySelector('.formregistro').addEventListener('submit', function(event) {
-    event.preventDefault();
+let email = document.querySelector('#emailregistro');
+let password = document.querySelector('#contraseñaregistro');
+let repeatPassword = document.querySelector('#repetircontraseñaregistro');
+let terms = document.querySelector('#terminosycondiciones');
 
-    
-    let email = document.querySelector('#emailregistro')
-    let password = document.querySelector('#contraseñaregistro')
-    let repeatPassword = document.querySelector('#repetircontraseñaregistro')
-    let terms = document.querySelector('#terminosycondiciones')
-    let errorMessage = document.querySelector('.errormensaje')
+let emailError = document.querySelector('#emailError');
+let passwordError = document.querySelector('#passwordError');
+let repeatPasswordError = document.querySelector('#repeatPasswordError');
+let termsError = document.querySelector('#termsError');
 
 
-    if (!email) {
-        errorMessage = document.querySelector("p").innerHTML = "Por favor complete el campo email";
+email.addEventListener('input', function() {
+    if (email.value === '') {
+        emailError.innerText = 'Por favor complete el campo email';
+    } else {
+        emailError.innerText = '';
+    }
+});
+
+password.addEventListener('input', function() {
+    if (password.value === '') {
+        passwordError.innerText = 'Por favor complete el campo contraseña';
+    } else if (password.value.length < 6) {
+        passwordError.innerText = 'Debe ingresar al menos 6 caracteres';
+    } else {
+        passwordError.innerText = '';
+    }
+});
+
+repeatPassword.addEventListener('input', function() {
+    if (repeatPassword.value !== password.value) {
+        repeatPasswordError.innerText = 'Las contraseñas no coinciden';
+    } else {
+        repeatPasswordError.innerText = '';
+    }
+});
+
+terms.addEventListener('blur', function() {
+    if (!terms.checked) {
+        termsError.innerText = 'Por favor acepte los términos y condiciones';
+    } else {
+        termsError.innerText = '';
+    }
+});
+
+form.addEventListener('submit', function(event) {
+    if (email.value === '') {
+        emailError.innerText = 'Por favor complete el campo email';
+        email.focus();
+        event.preventDefault();
         return;
     }
-    
-    if (!password) {
-        errorMessage = document.querySelector("p").innerHTML = "Por favor complete el campo contraseña";
+
+    if (password.value === '') {
+        passwordError.innerText = 'Por favor complete el campo contraseña';
+        password.focus();
+        event.preventDefault();
         return;
-    
-    }
-    if (password.length < 6) {
-        errorMessage = 'Debe ingresar al menos 6 caracteres';
+    } else if (password.value.length < 6) {
+        passwordError.innerText = 'Debe ingresar al menos 6 caracteres';
+        password.focus();
+        event.preventDefault();
         return;
     }
 
-    if (password !== repeatPassword) {
-        errorMessage = 'Las contraseñas no coinciden';
+    if (repeatPassword.value !== password.value) {
+        repeatPasswordError.innerText = 'Las contraseñas no coinciden';
+        repeatPassword.focus();
+        event.preventDefault();
         return;
     }
 
-    if (!terms) {
-        errorMessage = 'Por favor acepte los términos y condiciones';
+    if (!terms.checked) {
+        termsError.innerText = 'Por favor acepte los términos y condiciones';
+        terms.focus();
+        event.preventDefault();
         return;
     }
 
     window.location.href = './login.html';
-});
-
-
-
-
+})
