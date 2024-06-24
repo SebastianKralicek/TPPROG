3ac82e79c9cd75560158a9feba73c9eea6d57f79
biehay <bhaymesbiedma@udesa.edu.ar>
2024-06-24 19:44:00 -0300

    js categoria terminado

--- TRABAJO/js/categoria.js
+++ TRABAJO/js/categoria.js
@@ -1,54 +1,41 @@
+let queryLc = location.search
 console.log(location.search);
+let queryStringObj = new URLSearchParams(queryLc)
+let categoria = queryStringObj.get("categoria")
+let categoriaaa = document.querySelector("#categoriajs")
+console.log(categoria);
 
-let queryString = location.search
+fetch(`https://fakestoreapi.com/products/category/${categoria}`)
+            .then(function(response){
+             return response.json()
+            })
 
-let queryStringObj = new URLSearchParams(queryString)
 
-let category = queryStringObj.get("category")
-
-console.log(category);
-            
-    let producctt = document.querySelector(".contenedoresIndexuno")
-    let especifico="";
-         
-    fetch ('https://fakestoreapi.com/products/category/men\'s clothing')
-
-    .then (function(response){
-        return response.json();
-    })
-
-    .then (function(data){
-        console.log(data);
-especifico = <section class= "contenedoresIndexuno">
-        
-         for (let i=0; i <data.length;> i++){
-            let stock = data [i];
-            console.log(stock);
-            console.log(stock.title);
-            console.log(stock.image);
-            console.log(stock.description);
-           
-
-            especifico=especifico+  `
-            <article class="hombresCat">
-            <section class="productos">
+        .then (function(data){
+            console.log(data);
+            for(let i = 0; i < data.length; i++ ){
+                categoriaaa.innerHTML += 
+                `
+                <section class="productos">
                 <article class="descripcionprod">
-                    <h3 class="textotitulo">${data[i].title}</h3>   
-                    <a href="./producto.html?id=${data[i].id}"><img src="${data[i].image}" class="remeranegra"></a>
-            <section class="precioydesc">
-                <p class="precio1"><b>$${data[i].price}</b></p>
-                <article class="precio">
-                    <a href="./producto.html?id=${data[i].id}"><button class="botoncomprar" id="${data[i].id}">Saber Mas</button></a>
-                </article>
-            </section>
-                </article> `;
-}
-        
-            especifico=especifico+ </section>
-            producctt.innerHTML = especifico;
-        }
-    )
-    .catch (function(err){
-        console.log(err);
+                <h3 class="textotitulo">${data[i].title}</h3>  
+                <img src="${data[i].image}" class="remeranegra">
+        <section class="precioydesc">
+            <article class="desc">${data[i].description}</article>
+            <article class="precio1"><b>$${data[i].price}</b></article>
+            <article class="precio">
+                <a href="producto.html"><button class="botoncomprar">Saber Mas</button></a> 
+            </article>
+            </section> 
+            </article>
+        <a href="carrito.html" class="clickcompra"></a>
+                `
+    }
+       
+})
+.catch(function(err){
+    console.log(err);
+})
 
-    })
+let titulocat = document.querySelector('.tituloseccion')
+titulocat.innerHTML = `<h2>${categoria}</h2>`
